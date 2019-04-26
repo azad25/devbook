24f918a1d51641aba3340f74007e97d86982c6c3
Ferdous Azad <ferdousazad12@gmail.com>
2019-04-26 08:37:25 +0600

    add experience validation fix

--- validator/experience.js
+++ validator/experience.js
@@ -6,10 +6,12 @@ module.exports = validateExperienceInput = data => {
 
   data.title = !is_empty(data.title) ? data.title : "";
   data.company = !is_empty(data.company) ? data.company : "";
+  data.location = !is_empty(data.location) ? data.location : "";
   data.from = !is_empty(data.from) ? data.from : "";
 
   if (validator.isEmpty(data.title)) errors.title = "Job title is required";
   if (validator.isEmpty(data.company)) errors.company = "Company is required";
+  if (validator.isEmpty(data.location)) errors.location = "Company Location is required";
   if (validator.isEmpty(data.from)) errors.from = "From date is required";
 
   return {
